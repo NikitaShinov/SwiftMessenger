468fe478d69c83be1c1b92b56c58677b5d7577ff
Nikita Shinov <shinov_96@mail.ru>
2021-12-15 19:48:08 +0300

    setup UI

---
+++ SwiftMessenger/Controllers/ConverstionsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  SwiftMessenger
+//
+//  Created by max on 15.12.2021.
+//
+
+import UIKit
+
+class ConverstionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+
+
+}
+
