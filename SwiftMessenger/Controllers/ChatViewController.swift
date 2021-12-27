1368b9478254cda3a2502cbd17ce50d3c741939d
Nikita Shinov <shinov_96@mail.ru>
2021-12-27 15:30:00 +0300

    start set up new message functions

--- SwiftMessenger/Controllers/ChatViewController.swift
+++ SwiftMessenger/Controllers/ChatViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import MessageKit
+import InputBarAccessoryView
 
 struct Message: MessageType {
     var sender: SenderType
@@ -23,30 +24,60 @@ struct Sender: SenderType {
 
 class ChatViewController: MessagesViewController {
     
+    public let otherUserEmail: String
+    
+    public var isNewConversation = false
+    
     private var messages = [Message]()
     
     private let selfSender = Sender(photoURL: "",
                                     senderId: "1",
                                     displayName: "Cam Watkins")
-
+    
+    init(with email: String) {
+        self.otherUserEmail = email
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        messages.append(Message(sender: selfSender,
-                                messageId: "1",
-                                sentDate: Date(),
-                                kind: .text("Hello Mate! Hello Mate! Hello Mate! Hello Mate!")))
-        
         
         view.backgroundColor = .green
         
         messagesCollectionView.messagesDataSource = self
         messagesCollectionView.messagesLayoutDelegate = self
         messagesCollectionView.messagesDisplayDelegate = self
+        messageInputBar.delegate = self
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        messageInputBar.inputTextView.becomeFirstResponder()
     }
 
 }
-
+extension ChatViewController: InputBarAccessoryViewDelegate {
+    
+    func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
+        guard !text.replacingOccurrences(of: " ", with: "").isEmpty else {
+            return
+        }
+        
+        print ("Sending: \(text)")
+        
+        //send message
+        if isNewConversation {
+            //create convo in database
+        } else {
+            // append to existing conversation data
+        }
+    }
+}
 
 extension ChatViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
     func currentSender() -> SenderType {
--- SwiftMessenger/Controllers/ConverstionsViewController.swift
+++ SwiftMessenger/Controllers/ConverstionsViewController.swift
@@ -44,8 +44,20 @@ class ConverstionsViewController: UIViewController {
     
     @objc private func didTapComposeButton() {
         let vc = NewConversationViewController()
+        vc.completion = { [weak self] result in
+            self?.createNewConversation(result: result)
+        }
         let navVC = UINavigationController(rootViewController: vc)
         present (navVC, animated: true)
+    }
+    
+    private func createNewConversation(result: [String: String]) {
+        guard let name = result["name"], let email = result["email"] else { return }
+        let vc = ChatViewController(with: email)
+        vc.isNewConversation = true
+        vc.title = name
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     override func viewDidLayoutSubviews() {
@@ -93,7 +105,7 @@ extension ConverstionsViewController: UITableViewDelegate, UITableViewDataSource
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        let vc = ChatViewController()
+        let vc = ChatViewController(with: "ajkfa@gmail.com")
         vc.title = "Mike Dean"
         vc.navigationItem.largeTitleDisplayMode = .never
         navigationController?.pushViewController(vc, animated: true)
